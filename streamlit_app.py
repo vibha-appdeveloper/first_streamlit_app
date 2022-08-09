642866f1d32a98a112849fda8eb908bc0c85ca40
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-09 11:42:30 -0400

    Update streamlit_app.py
    Fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streanlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale , spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
                 
