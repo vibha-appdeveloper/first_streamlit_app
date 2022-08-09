14d2249db5c1ef6f88615ee17d471204196573c6
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-09 17:16:07 -0400

    Update streamlit_app.py
    display fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,6 @@ streamlit.dataframe(my_fruit_list)
 
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
