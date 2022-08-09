d4199cb295c31191f62af794d9b4cab2affa0741
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-09 11:37:20 -0400

    Update streamlit_app.py
    Written, updated Breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streanlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale , spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+                
