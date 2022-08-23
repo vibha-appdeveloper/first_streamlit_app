a6572f42d858022fc298bcf3c51155244d36f7e5
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-22 21:22:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ streamlit.dataframe( fruits_to_show)
 
 #New Section to display fruityvise api response
 streamlit.header("Fruityvice Fruit Advice!")
-
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
