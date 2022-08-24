a8a59fd1fa0e422fb3a61f43ca4053c60f0a35b2
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-24 15:57:07 -0400

    Update streamlit_app.py
    adding jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+#Allow the end user to add a fruit to the list
+add_my_fruit = 'jackfruit'
+streamlit.write('Thanks for adding', add_my_fruit)
 
 
 
