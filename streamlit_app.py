2adc0e14aa5d6f640eed75be290b64142e7875c7
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-24 13:17:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-#snowflake connector
 
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+#snowflake connector
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -45,7 +48,5 @@ streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 
