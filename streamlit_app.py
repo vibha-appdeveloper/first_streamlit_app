cf634371a9f85e30b47fd49f31461ae7d95b6381
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-24 18:49:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding', add_my_fruit)
 
 #This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')" 
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 
 
