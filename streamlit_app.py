21df4b59584a5aeb8c19b51824b88f0d84589a47
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-25 14:18:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ def get_fruit_load_list():
          return my_cur.fetchall()
         
  #Add a button to load the fruit
-if streamlit.button('Get Fruit LOad List'):
+if streamlit.button('Get Fruit Load List'):
    
     my_data_rows = get_fruit_;oad_list()
     streamlit.dataframe(my_data_rows)
