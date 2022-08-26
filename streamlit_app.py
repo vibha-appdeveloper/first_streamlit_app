3d58666475ddcd3fcbec6c07016577c567b96886
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-26 17:43:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe( fruits_to_show)
 
+#Allow the end user to add a fruit to the list
+add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('Thanks for adding' , add_my_fruit)
+
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -58,9 +62,7 @@ if streamlit.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-#Allow the end user to add a fruit to the list
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
-streamlit.write('Thanks for adding' , add_my_fruit)
+
 
 
 
