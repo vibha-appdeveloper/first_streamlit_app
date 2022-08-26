e64ee5896c8a21a510a2e831893c2ada4f22dbe4
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-26 13:16:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
 add_my_fruit = ("What fruit would you like to add", 'Jackfruit')
-streamlit.write("Thanks for adding" , add_my_fruit)
+streamlit.write("Thanks for adding" , 'Jackfruit')
 
 
 
