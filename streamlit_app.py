6cc76de7b998f3543895a2a8ef8db32eb72c9c43
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-26 11:53:39 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def get_fruityvice_data(this_fruit_choice):
     return fruityvice_normalized
 
 #New Section to display fruityvise api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
