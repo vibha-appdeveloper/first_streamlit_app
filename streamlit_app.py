cffabb9372d1ce803f32197933bd33551ba21753
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-26 12:41:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def get_fruityvice_data(this_fruit_choice):
 #New Section to display fruityvise api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?') + ('kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?' + ('kiwi'))
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
   else:
