2ca5f11c2230d5ae4632dd2f8c85fbac16130e2f
vibha-appdeveloper <110851684+vibha-appdeveloper@users.noreply.github.com>
2022-08-26 12:34:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe( fruits_to_show)
 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 #New Section to display fruityvise api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -50,12 +56,6 @@ def get_fruit_load_list():
 
  #don't run anything past here while we troubleshoot
 streamlit.stop()
-
-#create the repeatable code block (called a function)
-#def get_fruityvice_data(this_fruit_choice):
-   # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-  #  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   # return fruityvice_normalized
 
 
 import requests
